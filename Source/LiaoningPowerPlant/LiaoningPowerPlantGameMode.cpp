92fa3a4b58f72dbf51d41ee9deb8a375eb30356e
PiangGG <974805475@qq.com>
2021-09-13 23:29:22 +0800

    加入动态天空盒子

--- Source/LiaoningPowerPlant/LiaoningPowerPlantGameMode.cpp
+++ Source/LiaoningPowerPlant/LiaoningPowerPlantGameMode.cpp
@@ -7,7 +7,7 @@
 ALiaoningPowerPlantGameMode::ALiaoningPowerPlantGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("Blueprint'/Game/ThirdPakage/ThirdPersonCPP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter_C'"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
